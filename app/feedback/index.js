a851e8dff363d3b4ca8f94c25cb491aa0b2c2b28
Jon Heslop <jon@jonheslop.com>
2019-04-17 17:06:51 +0100

    Base for feedback page

---
+++ app/feedback/index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+// NPM dependencies
+const express = require('express')
+
+// Local dependencies
+const getController = require('./get.controller')
+const postController = require('./post.controller')
+
+// Initialisation
+const router = new express.Router()
+const paths = {
+  index: '/ask-a-question'
+}
+
+// Routing
+router.get(paths.index, getController)
+router.post(paths.index, postController)
+
+// Export
+module.exports = {
+  router,
+  paths
+}
--- app/router.js
+++ app/router.js
@@ -5,6 +5,7 @@ const healthcheck = require('./healthcheck')
 const index = require('./index')
 const somethingsWrong = require('./somethings-wrong')
 const help = require('./help')
+const feedback = require('./feedback')
 
 // Export
 module.exports.bind = app => {
@@ -12,4 +13,5 @@ module.exports.bind = app => {
   app.use(index.router)
   app.use(somethingsWrong.router)
   app.use(help.router)
+  app.use(feedback.router)
 }
