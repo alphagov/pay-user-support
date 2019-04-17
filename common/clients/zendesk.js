ed00990e4ff313e046b152482959a43e2962be05
Jon Heslop <jon@jonheslop.com>
2019-04-17 17:10:50 +0100

    Adding Zendesk Client
    Just ripped it from pay-selfservice

---
+++ common/clients/zendesk.js
@@ -0,0 +1,43 @@
+'use strict'
+
+// NPM dependencies
+const zendesk = require('node-zendesk')
+const logger = require('pino')
+
+const zendeskConfig = require('../config/zendesk')
+
+const zendeskClient = zendesk.createClient({
+  username: process.env.ZENDESK_USER,
+  token: process.env.ZENDESK_API_KEY,
+  remoteUri: process.env.ZENDESK_URL,
+  proxy: process.env.http_proxy
+})
+
+module.exports = {
+  createTicket: opts => {
+    return new Promise(function (resolve, reject) {
+      zendeskClient.tickets.create({
+        ticket: {
+          requester: {
+            email: opts.email,
+            name: opts.name
+          },
+          type: opts.type,
+          subject: opts.subject,
+          comment: {
+            body: opts.message
+          },
+          group_id: zendeskConfig.GROUP_ID,
+          organization_id: zendeskConfig.ORG_ID,
+          tags: opts.tags
+        }
+      }, (err, request, result) => {
+        if (err) {
+          logger('error', `${opts.correlationId} there was an error creating Zendesk ticket: ${err}`)
+          reject(new Error(`Something went wrong, please contact support team.`))
+        }
+        resolve()
+      })
+    })
+  }
+}
---
+++ common/config/zendesk.js
@@ -0,0 +1,4 @@
+module.exports = {
+  GROUP_ID: 20487877,
+  ORG_ID: 21891972
+}
