7a2322a63b24fa65ae63f81ddd839a717d6e011d
Jon Heslop <jon@jonheslop.com>
2019-04-18 16:18:34 +0100

    tests for help page

---
+++ test/integration/help.spec.js
@@ -0,0 +1,54 @@
+describe('Help page', () => {
+  it('should form for user to report a problem', () => {
+    cy.visit('/help')
+
+    cy.title().should('eq', 'Help using GOV.UK Pay - Support - GOV.UK Pay')
+    cy.get('#message').should('be.visible')
+    cy.get('#name').should('be.visible')
+    cy.get('#email').should('be.visible')
+  })
+
+  it('should show errors if user leaves everything blank', () => {
+    cy.get('.govuk-button').click()
+    cy.location('pathname').should('eq', `/help`)
+
+    cy.get('.govuk-error-summary').should('be.visible')
+    cy.get('.govuk-error-summary__list li').should('have.length', 3)
+
+    cy.get('.govuk-form-group--error').should('have.length', 3)
+    cy.get('.govuk-error-message').should('have.length', 3)
+    cy.get('.govuk-error-message').eq(0).should('contain', 'This field is required')
+    cy.get('.govuk-error-message').eq(1).should('contain', 'This field is required')
+    cy.get('.govuk-error-message').eq(2).should('contain', 'Please use a valid email address')
+  })
+
+  it('should show errors if invalid email', () => {
+    cy.visit('/help')
+
+    cy.get('#message').type('Hello world')
+    cy.get('#name').type('Some Name')
+    cy.get('#email').type('your.name@localhost')
+
+    cy.get('.govuk-button').click()
+    cy.location('pathname').should('eq', `/help`)
+
+    cy.get('.govuk-error-summary').should('be.visible')
+    cy.get('.govuk-error-summary__list li').should('have.length', 1)
+
+    cy.get('.govuk-form-group--error').should('have.length', 1)
+    cy.get('.govuk-error-message').should('have.length', 1)
+    cy.get('.govuk-error-message').should('contain', 'Please use a valid email address')
+  })
+
+  it('should submit successfully with a message', () => {
+    cy.visit('/help')
+
+    cy.get('#message').type('Hello world')
+    cy.get('#name').type('Some Name')
+    cy.get('#email').type('your.name@email.com')
+
+    cy.get('.govuk-button').click()
+    cy.location('pathname').should('eq', `/help`)
+    cy.get('.info-box').should('be.visible')
+  })
+})
