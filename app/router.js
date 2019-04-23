ee828ed5dbf793498dc01a438be567ede7383a3d
Jon Heslop <jon@jonheslop.com>
2019-04-23 15:00:03 +0100

    Direct debit pages

--- app/router.js
+++ app/router.js
@@ -11,6 +11,7 @@ const features = require('./features')
 const roadmap = require('./roadmap')
 const security = require('./security')
 const psp = require('./payment-service-provider')
+const directDebit = require('./direct-debit')
 
 // Export
 module.exports.bind = app => {
@@ -22,5 +23,7 @@ module.exports.bind = app => {
   app.use(success.router)
   app.use(features.router)
   app.use(roadmap.router)
+  app.use(security.router)
   app.use(psp.router)
+  app.use(directDebit.router)
 }
